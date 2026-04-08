fa9201a6401c44daa74421a832778abfbeb2dfa0
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2026-04-08 16:58:34 +0200

    add qt style sheet

--- src/dalikam/main.py
+++ src/dalikam/main.py
@@ -4,6 +4,9 @@ from dalikam.ui.landingPage.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
+    with open("src/dalikam/style.qss", "r") as f:
+        _style = f.read()
+        app.setStyleSheet(_style)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
