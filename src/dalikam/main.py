f0584931e4d113176f4ba4584aec855e6e46b399
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2026-07-07 14:29:00 +0200

    fix: reference QT stylesheet using dynamic paths

--- src/dalikam/main.py
+++ src/dalikam/main.py
@@ -1,10 +1,13 @@
 import sys
+from pathlib import Path
+
 from PyQt6.QtWidgets import QApplication
 from dalikam.ui.landingPage.main_window import MainWindow
 
 def main():
+    style_path = Path(__file__).parent / "style.qss"
     app = QApplication(sys.argv)
-    with open("src/dalikam/style.qss", "r") as f:
+    with open(style_path, "r") as f:
         _style = f.read()
         app.setStyleSheet(_style)
     window = MainWindow()
